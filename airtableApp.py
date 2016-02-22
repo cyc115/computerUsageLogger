ee22337c83830a4a9ec630ed39eae16d7fea27de
yuechuan <yuechuan20@gmail.com>
2016-02-21 23:40:12 -0500

    basic function works
    the app can now run in a terminal and checks up on the window text every 3 seconds, if the window changed then an entry with the title of the window as well as start and end time is sent to the airtable backend (works beautifully)

---
+++ airtableApp.py
@@ -0,0 +1,63 @@
+
+from datetime import tzinfo, timedelta, datetime
+from collections import OrderedDict
+import json
+
+
+sTime = "start time"
+eTime = "end time"
+tags = "Tag"
+aName = "Application name"
+
+class Entry():
+
+    def __init__(self, name):
+        self.appName = name
+        self.tags = []
+
+    def buildEntry (self) :
+        '''
+        validate the object attributes and build the dictionary that can be used to
+        make the request to the airTable backend
+        :return: OrderedDict representation of the object
+        '''
+
+
+        if hasattr(self, 'sTime') and hasattr(self, 'eTime') \
+                and hasattr(self, 'tags') and hasattr(self, 'appName') :
+            # this can be processed and build into an ordered dict
+
+            d = {}
+            d[sTime] = self.sTime.isoformat()
+            d[eTime] = self.eTime.isoformat()
+            d[aName] = self.appName
+            d[tags] = self.tags
+
+            return OrderedDict(d)
+
+    def start(self):
+
+        '''
+        start the timer
+        :return: self : the Entry object
+        '''
+
+        self.sTime = datetime.now()
+        return self
+
+    def stop(self):
+
+        '''
+        stop the timer
+        :return: self : the Entry object
+        '''
+
+        self.eTime = datetime.now()
+        return self
+
+
+
+
+
+
+
---
+++ getWindow.py
@@ -0,0 +1,46 @@
+import subprocess
+import time
+import json 
+from airtable import Airtable
+from airtableApp import Entry
+
+#const
+airtableAPIKey = "keybBQanzhJwdlveV"
+airtableBaseId = "appjOjH6SqE5fiIWS"
+loggerTableName = 'Application'
+cmd = 'cat /proc/$(xdotool getwindowpid $(xdotool getwindowfocus))/comm'
+
+'''
+class variables
+'''
+exit = False
+at = Airtable(airtableBaseId, airtableAPIKey)
+
+'''
+class methods
+'''
+def getRunningProgram () :
+    output_byte = subprocess.check_output(cmd, shell=True)
+    return output_byte.decode('utf-8')[0:-1]
+
+
+prevApp = Entry(getRunningProgram()).start()
+
+print('current program ' , prevApp.appName)
+
+while not exit:
+    currProgram = getRunningProgram()
+    if currProgram != prevApp.appName:
+        prevApp.stop()
+        entryDict = prevApp.buildEntry()
+        prevApp = Entry(currProgram).start()
+
+        print('changed to ' , prevApp.appName, " update airtable")
+        #update air table
+        at.create(loggerTableName, entryDict)
+
+        print ('backend updated!')
+
+    #wait 10 seconds
+    time.sleep(3)
+
